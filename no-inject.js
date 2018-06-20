6d95c267bbf907544e847f32217f07a27c95db8b
Tobi Schäfer <interface@p3k.org>
2018-06-20 14:25:48 +0200

    Update no-inject.js

--- no-inject.js
+++ no-inject.js
@@ -2,7 +2,7 @@ define(function(require) {
   var less = require('less');
 
   var lessAPI = {
-    pluginBuilder: './no-inject-builder'
+    pluginBuilder: './less-no-inject-builder'
   };
 
   if (typeof window === 'undefined') {
